a9e07ebdbc190d8a63cf3f6b9713d1ae38ea37c4
schrifty <schrifty@gmail.com>
2025-04-23 08:40:50 -0500

    Updated branding

--- src/lib/components/icons/HamiltonLogo.svelte
+++ src/lib/components/icons/HamiltonLogo.svelte
@@ -7,6 +7,6 @@
 </script>
 
 <div class="flex flex-col items-start {classNames}">
-    <div class="text-lg font-semibold text-primary-500">Hamilton AI</div>
-    <div class="text-sm text-gray-600 dark:text-gray-400">Strategy Advisors</div>
+    <div class="text-lg font-semibold text-primary-500">Strategy Advisors</div>
+    <div class="text-sm text-gray-600 dark:text-gray-400">Contextual Chat</div>
 </div>
